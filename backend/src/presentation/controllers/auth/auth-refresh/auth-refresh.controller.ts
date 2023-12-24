5fb1868297a4332b345fe54d6489327c9c874017
Erlan <luciodevinfo@gmail.com>
2023-12-24 06:13:53 -0300

    feat: add controllers auth

---
+++ backend/src/presentation/controllers/auth/auth-refresh/auth-refresh.controller.ts
@@ -0,0 +1,18 @@
+import { IAuthRefresh } from '@/domain/usecases/auth'
+import { ok, serverError } from '@/presentation/helpers'
+import { Controller, HttpResponse, Request } from '@/presentation/protocols'
+
+export class AuthRefreshController implements Controller {
+  constructor(private readonly authRefreshUsecase: IAuthRefresh) {}
+
+  public async handle(request: Request): Promise<HttpResponse> {
+    try {
+      const params = request.body as IAuthRefresh.Params
+      const result = await this.authRefreshUsecase.authRefresh(params)
+
+      return ok(result)
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
---
+++ backend/src/presentation/controllers/auth/index.ts
@@ -0,0 +1,3 @@
+export * from './auth-login/auth-login.controller'
+export * from './auth-refresh/auth-refresh.controller'
+export * from './auth-register/auth-register.controller'
