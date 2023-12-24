37cf47238602477a308866cb1dc17548be202739
Erlan <luciodevinfo@gmail.com>
2023-12-24 06:18:12 -0300

    refactor: apply DRY principle

---
+++ backend/src/main/config/env.ts
@@ -0,0 +1,9 @@
+export default {
+  logLevel: process.env.LOG_LEVEL || 'debug',
+  corsOrigin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/flag-secops-hub',
+  dbName: process.env.DB_NAME || 'flag-secops-hub',
+  port: parseInt(process.env.PORT || '5000', 10),
+  jwtSecret: process.env.JWT_SECRET || 'er123lan_gbtech$%&*()_+',
+  cookieSecret: process.env.COOKIE_SECRET || 'lu123cio_gbtech$%&*()_+'
+}
