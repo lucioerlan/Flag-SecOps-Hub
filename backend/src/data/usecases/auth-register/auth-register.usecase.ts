44a1d40d51f8df38b66d9831e5ca4f2de627fa66
Erlan <luciodevinfo@gmail.com>
2023-12-24 05:57:39 -0300

    chore: remove old files

--- backend/src/data/usecases/auth-register/auth-register.usecase.ts
+++
@@ -1,7 +0,0 @@
-import { IAuthRegister } from '@/domain/usecases/auth-register/auth-register'
-
-export class AuthRegisterUsecase implements IAuthRegister {
-  async authRegister(params: IAuthRegister.Params): Promise<IAuthRegister.Result> {
-    return { message: `Registered user ${params.username}` }
-  }
-}
