46f195c9d4fb93fb6185c1eab2f71dd1bec40940
Erlan <luciodevinfo@gmail.com>
2023-12-25 05:36:42 -0300

    chore: project settings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,21 @@
-export default function App() {
-  return <p className="read-the-docs">Hello World</p>
+import store from '@/store'
+import { Provider as StoreProvider } from 'react-redux'
+import { useRoutes } from 'react-router-dom'
+
+import { GlobalStyle } from './components'
+import useSettings from './hooks/useSettings'
+import routes from './routes'
+
+const App = () => {
+  const { settings } = useSettings()
+  const routing = useRoutes(routes(settings.isLoggedIn))
+
+  return (
+    <>
+      <GlobalStyle />
+      <StoreProvider store={store}>{routing}</StoreProvider>
+    </>
+  )
 }
+
+export default App
