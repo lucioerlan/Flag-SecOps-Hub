2af015c83b3f1e37f5174a6b3403e9e8699b510a
Erlan <luciodevinfo@gmail.com>
2023-12-27 12:45:32 -0300

    feat: translating the site into different languages

--- frontend/src/translate/locale/pt-br/notifications.ts
+++ frontend/src/translate/locale/pt-br/notifications.ts
@@ -1,19 +1,33 @@
 const translations = {
   success: {
     loaded: 'Carregado com sucesso',
-    stored: 'Registrado com sucesso'
+    stored: 'Registrado com sucesso',
+    deleteFeatureFlag: 'Feature Flag deletada com sucesso',
+    updateFeatureFlag: 'Feature Flag atualizada com sucesso',
+    createFeatureFlag: 'Feature Flag criada com sucesso',
+    welcomeBack: 'Bem-vindo de volta',
+    wemissyou: 'Sentimos sua falta',
+    successAccountCreation: 'Tudo pronto',
+    letsdoit: 'Redirecionando para o login'
   },
   error: {
     submit: 'Houve um erro ao salvar os dados. Tente novamente.',
     stored: 'Houve um erro ao salvar os dados. Tente novamente.',
+    passwordMatch: 'As senhas não conferem',
     login: 'Ops! Lamentamos, mas aconteceu um erro!',
     required: {
       title: 'Oops... há campos com erro',
       message: 'Confira os campos preenchidos e tente novamente.'
-    }
+    },
+    updateFeatureFlag: 'Houve um erro ao atualizar a Feature Flag. Tente novamente.',
+    createFeatureFlag: 'Houve um erro ao criar a Feature Flag. Tente novamente.',
+    deleteFeatureFlag: 'Houve um erro ao deletar a Feature Flag. Tente novamente.'
   },
   warning: {
-    credentials: 'Oops... credenciais inválidas'
+    credentials: 'Oops... credenciais inválidas',
+    'Invalid email': 'Oops... não encontramos nenhum usuário com esse e-mail',
+    'Invalid password': 'Oops... a senha está incorreta',
+    'Already exists': 'Oops... já existe um usuário com esse e-mail'
   }
 }
 
