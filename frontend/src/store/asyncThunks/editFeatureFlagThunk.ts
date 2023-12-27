0637707efb5b74aba5b8d7117a2f91b7cbc4f30b
Erlan <luciodevinfo@gmail.com>
2023-12-27 13:11:28 -0300

    feat: creating thunk to edit a feature flag

---
+++ frontend/src/store/asyncThunks/editFeatureFlagThunk.ts
@@ -0,0 +1,23 @@
+import { updateFeatureFlag } from '@/services/feature-flags'
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import { toast } from 'react-toastify'
+
+interface EditFeatureFlagPayload {
+  id: string
+  name: string
+  description: string
+  state: boolean
+  translate: (key: string) => string
+}
+
+export const editFeatureFlagThunk = createAsyncThunk(
+  'featureFlags/edit',
+  async ({ id, name, description, state, translate }: EditFeatureFlagPayload) => {
+    try {
+      await updateFeatureFlag(id, { name, description, state })
+      toast.success(translate('success.updateFeatureFlag'))
+    } catch (error) {
+      toast.error(translate('error.updateFeatureFlag'))
+    }
+  }
+)
