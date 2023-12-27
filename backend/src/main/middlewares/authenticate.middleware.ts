54193a44e676e20d1d5ed51faf85f5e490c7a995
Erlan <luciodevinfo@gmail.com>
2023-12-27 13:06:58 -0300

    refactor: removing nextfuncion

--- backend/src/main/middlewares/authenticate.middleware.ts
+++ backend/src/main/middlewares/authenticate.middleware.ts
@@ -1,12 +1,12 @@
 import { IDecrypter } from '@/data/protocols/cryptography/jwt/jwt'
 import { MESSAGES } from '@/domain/entities'
-import { Request, Response, Done } from '@/presentation/protocols'
+import { Request, Response } from '@/presentation/protocols'
 import { HttpResponse, Middleware } from '@/presentation/protocols'
 
 export class EnsureAuthenticatedMiddleware implements Middleware {
   constructor(private readonly decrypter: IDecrypter) {}
 
-  async handle(request: Request, response: Response, next: Done): Promise<HttpResponse> {
+  async handle(request: Request, response: Response): Promise<HttpResponse> {
     const authorizationHeader = request.headers.authorization
 
     if (!authorizationHeader) {
@@ -16,7 +16,6 @@ export class EnsureAuthenticatedMiddleware implements Middleware {
     const [, token] = authorizationHeader.split(' ')
     try {
       await this.decrypter.decrypt(token)
-      next()
     } catch (error) {
       return response.status(401).send({ error: MESSAGES.tokenIsInvalid })
     }
