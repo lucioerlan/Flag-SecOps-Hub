19fa500ce8243547c1b862a7220e5941f1b3fa50
Erlan <luciodevinfo@gmail.com>
2023-12-27 13:03:44 -0300

    feat: add new page register

--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -1,32 +1,43 @@
-import DashboardLayout from '@/layouts/DashboardLayout'
-import MainLayout from '@/layouts/MainLayout'
-import DashboardView from '@/pages/Dashboard'
+import HomeLayout from '@/layouts/HomeLayout'
+import HomeView from '@/pages/Home'
 import LoginView from '@/pages/Login'
 import NotFoundView from '@/pages/NotFound'
+import RegisterView from '@/pages/Register'
 import { Navigate } from 'react-router-dom'
 
-const routes = (isLoggedIn: boolean) => [
-  {
-    path: '/app',
-    element: isLoggedIn ? <DashboardLayout /> : <Navigate to="/login" />,
-    children: [
-      {
-        path: 'dashboard',
-        element: isLoggedIn ? <DashboardView /> : <Navigate to="/login" />
-      },
-      { path: '*', element: <Navigate to="/404" /> }
-    ]
-  },
-  {
-    path: '/',
-    element: <MainLayout />,
-    children: [
-      { path: 'login', element: <LoginView /> },
-      { path: '404', element: <NotFoundView /> },
-      { path: '/', element: <Navigate to="/app/dashboard" /> },
-      { path: '*', element: <Navigate to="/404" /> }
-    ]
-  }
-]
+const routes = (isLoggedIn: boolean) => {
+  const commonRoutes = [
+    {
+      path: '/',
+      element: isLoggedIn ? <Navigate to="/home" replace /> : <Navigate to="/login" replace />
+    },
+    {
+      path: '/login',
+      element: isLoggedIn ? <Navigate to="/home" replace /> : <LoginView />
+    },
+    {
+      path: '/register',
+      element: isLoggedIn ? <Navigate to="/home" replace /> : <RegisterView />
+    },
+    {
+      path: '*',
+      element: !isLoggedIn ? <Navigate to="/login" replace /> : <Navigate to="/404" replace />
+    }
+  ]
+
+  const authenticatedRoutes = [
+    {
+      path: '/',
+      element: <HomeLayout />,
+      children: [
+        { path: 'home', element: <HomeView /> },
+        { path: '404', element: <NotFoundView /> },
+        { path: '*', element: <Navigate to="/404" replace /> }
+      ]
+    }
+  ]
+
+  return isLoggedIn ? [...commonRoutes, ...authenticatedRoutes] : commonRoutes
+}
 
 export default routes
