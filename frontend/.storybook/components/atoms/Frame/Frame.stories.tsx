3c1f4e6e602f93b7876cd532fe2ce6622dcf8609
Erlan <luciodevinfo@gmail.com>
2023-12-28 15:24:16 -0300

    feat: adding documentation for atoms components

---
+++ frontend/.storybook/components/atoms/Frame/Frame.stories.tsx
@@ -0,0 +1,16 @@
+import { Block } from '../../../../src/components/atoms/Frame'
+
+export default {
+  title: 'Components/Atoms/Block',
+  component: Block,
+  parameters: {
+    layout: 'centered'
+  },
+  tags: ['autodocs']
+}
+
+export const Primary = {
+  args: {
+    children: 'Block'
+  }
+}
---
+++ frontend/.storybook/components/atoms/Spinner/Spinner.stories.tsx
@@ -0,0 +1,16 @@
+import { Spinner } from '../../../../src/components/atoms/Spinner'
+
+export default {
+  title: 'Components/Atoms/Spinner',
+  component: Spinner,
+  parameters: {
+    layout: 'centered'
+  },
+  tags: ['autodocs']
+}
+
+export const Primary = {
+  args: {
+    color: '#000'
+  }
+}
