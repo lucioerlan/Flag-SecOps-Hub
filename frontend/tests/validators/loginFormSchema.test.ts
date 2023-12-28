81e61c948a946ed0e3bbaa6f5b85f86e4e08622e
Erlan <luciodevinfo@gmail.com>
2023-12-28 15:30:07 -0300

    test: adding tests to validate forms

---
+++ frontend/tests/validators/loginFormSchema.test.ts
@@ -0,0 +1,36 @@
+import { LoginSchema } from '@/validators'
+import { ObjectSchema } from 'yup'
+
+import { messages } from './__mocks__/messages'
+
+jest.mock('@/hooks/useI18n', () => ({
+  useI18n: () => (key: string) => {
+    return messages[key] || key
+  }
+}))
+
+describe('LoginSchema', () => {
+  let loginSchema: ObjectSchema<{
+    email: string
+    password: string
+  }>
+
+  beforeAll(() => {
+    loginSchema = LoginSchema()
+  })
+
+  it('should validate a valid input', async () => {
+    const validInput = { email: 'test@example.com', password: '123456' }
+    await expect(loginSchema.isValid(validInput)).resolves.toBe(true)
+  })
+
+  it('should reject an invalid email', async () => {
+    const invalidInput = { email: 'test', password: '123456' }
+    await expect(loginSchema.validate(invalidInput)).rejects.toThrow('Invalid email format.')
+  })
+
+  it('should reject a short password', async () => {
+    const invalidInput = { email: 'test@example.com', password: '123' }
+    await expect(loginSchema.validate(invalidInput)).rejects.toThrow('Password must be at least 6 characters.')
+  })
+})
