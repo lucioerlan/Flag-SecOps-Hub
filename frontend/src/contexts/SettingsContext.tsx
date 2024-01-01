8048857e121c8b5ea4152047b2aa4e1f61516ae7
Erlan <luciodevinfo@gmail.com>
2024-01-01 04:51:30 -0300

    refactor: improving code readability

--- frontend/src/contexts/SettingsContext.tsx
+++ frontend/src/contexts/SettingsContext.tsx
@@ -1,34 +1,28 @@
 import { storeSettings } from '@/utils'
 import { createContext, useState, ReactNode } from 'react'
 
+export type Settings = {
+  accessToken: string
+  isLoggedIn: boolean
+}
+
 const SettingsContext = createContext({
   settings: {
     accessToken: '',
     isLoggedIn: false
   },
-  setSettings: (settings: object) => {
-    settings
-  }
+  setSettings: (settings: object) => {}
 })
 
-const defaultSettings = {
+export const defaultSettings = {
   accessToken: '',
   isLoggedIn: false
 }
 
-export function SettingsProvider({
-  settings,
-  children
-}: {
-  settings: {
-    accessToken: string
-    isLoggedIn: boolean
-  }
-  children: ReactNode
-}) {
+export function SettingsProvider({ settings, children }: { settings: Settings; children: ReactNode }) {
   const [currentSettings, setCurrentSettings] = useState(settings || defaultSettings)
 
-  const handleSaveSettings = (updatedSettings = {}) => {
+  const handleSaveSettings = (updatedSettings: object) => {
     const mergedSettings = { ...currentSettings, ...updatedSettings }
     setCurrentSettings(mergedSettings)
     storeSettings(mergedSettings)
